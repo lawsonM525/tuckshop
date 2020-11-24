5c43aefc400efbadea5a385f52cce7a773fb1e5c
Michelle Lawson <lawson_m@soshgic.edu.gh>
2020-11-24 17:55:05 +0000

    creation of user account model

--- Tuckshopapp/Tuckshop/account/models.py
+++ Tuckshopapp/Tuckshop/account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    hostel = models.CharField(max_length=255)
+    room = models.IntegerField()
+    balance = models.FloatField()
